459874f9da9eba4d3eefb535e2918a11d1c89829
Mishari Alharbi <mishari713@gmail.com>
2025-05-13 19:57:31 +0300

    Added a GUI for a better UX

--- main.java
+++ main.java
@@ -1,7 +1,11 @@
-private String word;
+import javax.swing.*;
+import java.awt.*;
+
+private final JFrame frame = new JFrame("Scrabble game");
 private HashMap<Character, Integer> map;
 private int score;
 public void main() {
+    GUIStart();
     map = new HashMap<>();
     for (char c : new char[]{'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'}) {
         map.put(c, 1);
@@ -20,18 +24,18 @@ public void main() {
     map.put('Q', 10);
     map.put('Z', 10);
     map.put(' ', 0);
-
-    System.out.println("----------------------- Welcome to the Scrabble Game -----------------");
-    System.out.print("Please Enter a word to calculate its score: ");
-    Scanner sc = new Scanner(System.in);
-    word = sc.nextLine();
-    while (hasNonLetters(word)){
-        System.out.print("Please enter a word with only letters: ");
-        word = sc.nextLine();
-    }
-    word = word.toUpperCase();
-    scrabbleCalculate(word);
-    System.out.println("Your word score is: " + score);
+//
+//    System.out.println("----------------------- Welcome to the Scrabble Game -----------------");
+//    System.out.print("Please Enter a word to calculate its score: ");
+//    Scanner sc = new Scanner(System.in);
+//    String word = sc.nextLine();
+//    while (hasNonLetters(word)){
+//        System.out.print("Please enter a word with only letters: ");
+//        word = sc.nextLine();
+//    }
+//    word = word.toUpperCase();
+//    scrabbleCalculate(word);
+//    System.out.println("Your word score is: " + score);
 
 }
 public static boolean hasNonLetters(String s) {
@@ -40,8 +44,52 @@ public static boolean hasNonLetters(String s) {
 
 
 private void scrabbleCalculate(String word){
-
+    word = word.toUpperCase();
     for (int i = 0; i < word.length(); i++){
         score +=(map.get(word.charAt(i)));
     }
+}
+
+private void GUIStart(){
+    frame.setSize(420, 250);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setResizable(false);
+    JLabel titleLabel = new JLabel("Please enter a word to calculate its score:");
+    JLabel resultLabel = new JLabel("");
+    JTextField textField = new JTextField(16);
+    JButton button = new JButton("Calculate");
+    JPanel panel = new JPanel();
+    panel.add(titleLabel);
+    panel.add(textField);
+    panel.add(button);
+    panel.add(resultLabel);
+    frame.add(panel);
+    button.addActionListener(e -> {
+        String word = textField.getText();
+        if(hasNonLetters(word)){
+            resultLabel.setText("Please enter a word with letters only");
+            resultLabel.setForeground(Color.red);
+        }
+        else{
+            scrabbleCalculate(word);
+            resultLabel.setText("                 Your score is: " + score);
+            resultLabel.setForeground(Color.blue);
+            score = 0;
+        }
+    });
+
+
+    centerFrame();
+    frame.setVisible(true);
+}
+
+private void centerFrame() {
+    Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+    int w = frame.getWidth();
+    int h = frame.getHeight();
+    int x = (dim.width - w)/2;
+    int y = (dim.height - h)/2;
+
+    frame.setLocation(x, y);
 }
